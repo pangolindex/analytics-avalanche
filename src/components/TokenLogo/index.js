7b7917972515d33cea5ba08b9a6b472b49212815
Connor Daly <connor.daly@avalabs.org>
2021-02-09 08:37:14 -0500

    Switched to new tokenlist and imgs

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -70,7 +70,7 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
     )
   }
 
-  const path = `https://raw.githubusercontent.com/dasconnor/tokenlist/main/imgs/${isAddress(
+  const path = `https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/${isAddress(
     address
   )}/logo.png`
 
