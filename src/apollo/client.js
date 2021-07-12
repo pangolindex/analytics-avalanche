4115f775ddeaf3de6d164fee5aaf922e2187b369
Brandon <brandonmino@bellsouth.net>
2021-07-11 21:10:29 -0400

    Use dasconnor/pangolin-dex subgraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/bmino/pangolin-beta',
+    uri: 'https://api.thegraph.com/subgraphs/name/dasconnor/pangolin-dex',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
