01d00c29c8e4651777994b5da5d18e2fc8d11bef
Brandon <brandon@pangolin.exchange>
2021-08-09 18:05:21 -0400

    Track top 100 LPs of top 100 pairs

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -141,7 +141,7 @@ export const PRICES_BY_BLOCK = (tokenAddress, blocks) => {
 
 export const TOP_LPS_PER_PAIRS = gql`
   query lps($pair: Bytes!) {
-    liquidityPositions(where: { pair: $pair }, orderBy: liquidityTokenBalance, orderDirection: desc, first: 10) {
+    liquidityPositions(where: { pair: $pair }, orderBy: liquidityTokenBalance, orderDirection: desc, first: 100) {
       user {
         id
       }
