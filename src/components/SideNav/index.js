4b7e4d1ce713da836e0e6a69b7136a4d22ed961e
nedsa548 <78696251+nedsa548@users.noreply.github.com>
2022-03-19 22:05:52 +0300

    update discord link

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -169,7 +169,7 @@ function SideNav({ history }) {
               </Link>
             </HeaderText>
             <HeaderText>
-              <Link href="https://twitter.com/pangolindex" target="_blank">
+              <Link href="https://discord.com/pangolindex" target="_blank">
                 Discord
               </Link>
             </HeaderText>
