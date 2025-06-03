eb4e0345c10c5c9647f533810dd779a6a0bb5f04
Canary Dev <dev@canary.exchange>
2025-06-03 15:14:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ if (typeof GOOGLE_ANALYTICS_ID === 'string') {
     customBrowserType: !isMobile
       ? 'desktop'
       : 'web3' in window || 'ethereum' in window
-      ? 'mobileWeb3'
-      : 'mobileRegular',
+        ? 'mobileWeb3'
+        : 'mobileRegular',
   })
 } else {
   ReactGA.initialize('test', { testMode: true, debug: true })
 }
 
-function ContextProviders({ children }) {
+function ContextProviders({ children }) {//ss
   return (
     <LocalStorageContextProvider>
       <ApplicationContextProvider>
